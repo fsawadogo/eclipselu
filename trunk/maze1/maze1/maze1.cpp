11af3d0e044420c418061fc5a63c362fa9c7cccd
eclipselu <eclipselu@bf7c4ae8-0690-11df-8158-c7cbc0dd6240>
2010-05-07 10:11:55 +0000

    issue: results are different with different compilers g++ and vc9.0

--- trunk/maze1/maze1/maze1.cpp
+++ trunk/maze1/maze1/maze1.cpp
@@ -7,16 +7,17 @@ LANG: C++
 #include <cstdio>
 #include <cstring>
 #include <climits>
-#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 const int infinity = 99999999;
 
 int w,h;
-char maze[2*38+1][2*100+1];
-int g[38][100][38][100];
-int exits[3800][2],ecnt=0;
+char maze[2*100+1][2*38+1];
+int exits[2][2],ecnt=0;
+int dist1[100][38];
+int dist2[100][38];
 int dir[4][2] = {{-1,0},{0,1},{1,0},{0,-1}};   
 
 bool isexit(int i,int j,int d)
@@ -33,16 +34,15 @@ bool isexit(int i,int j,int d)
 
 void init()		//input data and create graph
 {
-	int i,j,k,l;
-	int x,y,x1,y1;
+	int i,j,k;
+	int x,y;
 	scanf("%d%d\n",&w,&h);
-	for (i=0;i<2*h+1;i++)
-		fgets(maze[i],201,stdin);
-	for (i=0;i<h;i++)
-		for (j=0;j<w;j++)
-			for (k=0;k<h;k++)
-				for (l=0;l<w;l++)
-					g[i][j][k][l]=infinity;
+	for (i=0;i<h*2+1;i++)
+	{
+		for (j=0;j<w*2+1;j++)
+			maze[i][j]=getchar();
+		getchar();
+	}
 	for (i=0;i<h;i++)
 		for (j=0;j<w;j++)
 		{
@@ -51,31 +51,18 @@ void init()		//input data and create graph
 				x=2*i+1+dir[k][0];
 				y=2*j+1+dir[k][1];
 				bool used = false;
-				if (maze[x][y]==' ')
+				if (maze[x][y]==' '&&!used&&isexit(i,j,k))
 				{
-					if (isexit(i,j,k))
-					{
-						if (!used)
-						{
-
-							exits[ecnt][0]=i;
-							exits[ecnt][1]=j;
-							ecnt++;
-							used=true;	
-						}
-					}
-					else
-					{
-						x1=i+dir[k][0];
-						y1=j+dir[k][1];
-						g[i][j][x1][y1]=g[x1][y1][i][j]=1;
-					}
+					exits[ecnt][0]=i;
+					exits[ecnt][1]=j;
+					ecnt++;
+					used=true;											
 				}
 			}
 		}
 }
 
-void getmin(int &x,int &y,int dist[38][100],bool vis[38][100])
+void getmin(int &x,int &y,int dist[100][38],bool vis[100][38])
 {
 	int mincost = INT_MAX;
 	int i,j;
@@ -89,12 +76,12 @@ void getmin(int &x,int &y,int dist[38][100],bool vis[38][100])
 			}
 }
 
-int dijkstra(int srcx, int srcy, int destx, int desty)
+void dijkstra(int srcx, int srcy,int distance[100][38])
 {
-	int distance[38][100];
-	bool visited[38][100];
-	int i,j,k,l;
-	int x,y;
+	bool visited[100][38];
+	int i,j,k;
+	int x,y,x1,y1;
+
 	for (i=0;i<h;i++)
 		for (j=0;j<w;j++)
 		{
@@ -108,31 +95,33 @@ int dijkstra(int srcx, int srcy, int destx, int desty)
 		{
 			getmin(x,y,distance,visited);
 			visited[x][y]=true;
-			for (k=0;k<h;k++)
-				for (l=0;l<w;l++)
-					if (!visited[k][l]&&g[x][y][k][l]<infinity
-						&&distance[x][y]+g[x][y][k][l]<distance[k][l])
-						distance[k][l] = distance[x][y]+g[x][y][k][l];			
-		}
-	return distance[destx][desty];
+			for (k=0;k<4;k++)
+			{
+				x1=2*x+1+dir[k][0];
+				y1=2*y+1+dir[k][1];
+				if (x1>=0&&x1<2*h+1&&y1>=0&&y1<2*w+1)	//没在边界上
+					if (maze[x1][y1]==' ') //相通
+					{
+						x1=x+dir[k][0];
+						y1=y+dir[k][1];
+						if (!visited[x1][y1]&&distance[x][y]+1<distance[x1][y1])
+							distance[x1][y1]=distance[x][y]+1;
+					}	
+			}
+		}	
 }
 
 void solve()
 {
-	int i,j,k,d;
-	int ans = 0,minsteps;
+	int i,j,d;
+	int ans=0;
+	dijkstra(exits[0][0],exits[0][1],dist1);
+	dijkstra(exits[1][0],exits[1][1],dist2);
 	for (i=0;i<h;i++)
 		for (j=0;j<w;j++)
 		{
-			minsteps = infinity;
-			for (k=0;k<ecnt;k++)
-			{
-				d = dijkstra(i,j,exits[k][0],exits[k][1]);
-				if (d<minsteps)
-					minsteps=d;
-			}
-			if (minsteps>ans)
-				ans = minsteps;
+			d=min(dist1[i][j],dist2[i][j]);
+			if (d<infinity&&d>ans) ans=d;
 		}
 	printf("%d\n",ans+1);
 }
