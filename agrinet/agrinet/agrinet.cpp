39d2576a1616f1dabceab0a319662acbd33402ea
eclipselu <eclipselu@bf7c4ae8-0690-11df-8158-c7cbc0dd6240>
2010-05-12 07:54:37 +0000

    using Kruskal's algorithm

--- agrinet/agrinet/agrinet.cpp
+++ agrinet/agrinet/agrinet.cpp
@@ -5,85 +5,84 @@ LANG: C++
 */
 
 #include <cstdio>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
 
 const int maxn = 101;
 const int infinity = 99999999;
 
-int weight[maxn][maxn];
+struct edge
+{
+	int src,dest;
+	int cost;
+};
+
+int father[maxn];
+vector<struct edge> arr;
 int n;
+
+inline bool cmp(struct edge a,struct edge b)
+{
+	return a.cost<b.cost;
+}
+
+int getfather(int k)
+{
+	if (father[k]==k) return k;
+	father[k]=getfather(father[k]);
+	return father[k];
+}
 
 void init()
 {
-	int i,j;
-
+	int i,j,cost;
+	struct edge e;
 	scanf("%d",&n);
+
 	for (i=1;i<=n;i++)
 		for (j=1;j<=n;j++)
-			weight[i][j]=infinity;
+		{
+			scanf("%d",&cost);	
+			if (i<j)
+			{
+				e.src=i;
+				e.dest=j;
+				e.cost=cost;
+				arr.push_back(e);
+			}
+		}
+
+	sort(arr.begin(),arr.end(),cmp);
 	for (i=1;i<=n;i++)
-		for (j=1;j<=n;j++)
-			scanf("%d",&weight[i][j]);
+		father[i]=i;
 }
 
-//standard prim's algorithm
-void prim(int src)		//starts from node src
+void kruskal() 
 {
 	int i,j;
-	int distance[maxn];	//distance[j]: distance from tree to node j
-	int source[maxn];
-	bool intree[maxn];
-	int treesize,treecost;
+	int treecost=0,edgecount=0;
 
-	for (i=1;i<=n;i++)
+	for (i=0;i<int(arr.size());i++)
 	{
-		distance[i]=infinity;
-		intree[i]=false;
-		source[i]=-1;
-	}
-
-	treesize=1;
-	treecost=0;
-	intree[src]=true;
-
-	for (i=1;i<=n;i++)
-		if (i!=src&&weight[src][i]<infinity)
+		if (getfather(arr[i].src)!=getfather(arr[i].dest))
 		{
-			distance[i]=weight[src][i];
-			source[i]=src;
+			treecost+=arr[i].cost;
+			father[getfather(arr[i].src)]=getfather(arr[i].dest);
+			edgecount++;
 		}
-
-		while(treesize<n)
-		{
-			//find node with minimum distance to tree
-			int mincost = infinity;
-			for (j=1;j<=n;j++)
-				if (!intree[j]&&distance[j]<mincost)
-				{
-					mincost=distance[j];
-					i=j;
-				}
-
-				//add edge source[i],i to tree
-				treesize++;
-				treecost+=distance[i];
-				intree[i]=true;
-
-				//update distance
-				for (j=1;j<=n;j++)
-					if (!intree[j]&&weight[i][j]<infinity&&weight[i][j]<distance[j])
-					{
-						distance[j]=weight[i][j];
-						source[j]=i;
-					}
-		}
-
-		printf("%d\n",treecost);
+		if (edgecount==n-1)
+			break;
+	}	
+	printf("%d\n",treecost);
 }
+
 int main()
 {
 	freopen("agrinet.in","r",stdin);
 	freopen("agrinet.out","w",stdout);
 	init();
-	prim(1);
+	kruskal();
 	return 0;
 }
